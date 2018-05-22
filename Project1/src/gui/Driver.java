01bb214dc9fa4e6bf62eb0fe2c21e4bf270ccb16
Cody <codyaroberson@outlook.com>
2018-05-22 12:14:53 -0700

    Implemented Driver and updated pkg info

--- Project1/src/gui/Driver.java
+++ Project1/src/gui/Driver.java
@@ -1,10 +1,43 @@
 package gui;
 
+<<<<<<< HEAD
+import java.awt.EventQueue;
+
+/**
+ * 
+ * @author cody
+ * <h2>Driver for GUI</h2>
+ * <p>This class is used to make the GUI runnable but also modular.
+ * That way, the gui made in this project can be called on in the future
+ * by a larger project.</p>
+ * <p>Calls DrawFrame</p>
+ */
+
+public class Driver {
+
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run() 
+			{
+				try 
+				{
+					new DrawFrame();
+				} 
+				catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+=======
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+>>>>>>> d7997d6d3719de0865f795e5e9cb46133280cd25
 	}
 
 }
--- Project1/src/gui/package-info.java
+++ Project1/src/gui/package-info.java
@@ -2,7 +2,12 @@
  * 
  */
 /**
+<<<<<<< HEAD
+ * Contains the gui classes for the semester project
+ * @author Cody
+=======
  * @author Cody
  * Contains the gui classes for the semester project
+>>>>>>> d7997d6d3719de0865f795e5e9cb46133280cd25
  */
 package gui;
