a6fd57a1ddf2f35c37430a573317fa7dacd97785
spencer-g <spencer.e.griffin@gmail.com>
2018-05-23 23:31:39 -0700

    Updated DrawPanelOne
    DrawPanelOne updated with Testing driver class and supporting Label classes.

---
+++ Project1/src/gui/OvalLabel.java
@@ -0,0 +1,20 @@
+package gui;
+import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+
+public class OvalLabel extends JLabel{
+   public OvalLabel(){
+      super();
+      super.setVisible(true);
+      super.setOpaque(true);
+      super.setPreferredSize(new Dimension(70, 70));
+   }
+   
+   public void paintComponent(Graphics g){
+      super.paintComponent(g);
+      g.setColor(Color.BLACK);
+      g.fillOval(10, 10, 50, 50);
+   }
+}
