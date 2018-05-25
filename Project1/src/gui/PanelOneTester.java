03168b6df88d477ea09dcf27b2348b286d9ced8a
Andrew Durkiewicz <andrewdurkiewicz@gmail.com>
2018-05-24 23:56:00 -0700

    I fixed the program. It was broken or something. Im working on finishing soon. I have several ideas

--- Project1/src/gui/PanelOneTester.java
+++ Project1/src/gui/PanelOneTester.java
@@ -1,23 +1,36 @@
-	package gui;
+package gui;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Color;
+import java.awt.Dimension;
+
 import gui.DrawPanelOne;
 
 public class PanelOneTester{
    public static void main(String[] args){
+	   //make the frame
       JFrame myFrame = new JFrame();
+      JPanel panel1 = new DrawPanelOne();//NOTE: REMOVED INT HEIGHT AND WIDTH FROM DRAWPANELONE
+      JPanel panel2 = new JPanel();
+      
+      myFrame.setTitle("Project1");
       myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       myFrame.setVisible(true);
-      myFrame.setBounds(0,0,800,600);
-      myFrame.setTitle("Hope it Works");
+      myFrame.setSize(new Dimension(800,600));
       
-      JPanel myPanel = new DrawPanelOne();//NOTE: REMOVED INT HEIGHT AND WIDTH FROM DRAWPANELONE
-      JPanel panel2 = new JPanel();
-      panel2.setVisible(true);
       panel2.setBackground(Color.RED);
-      myFrame.add(myPanel);
+      panel1.setBackground(Color.YELLOW);
+      myFrame.add(panel1);
       myFrame.add(panel2);
-      myPanel.setBounds(0,0,800,100);
+      
+      
+      
+      panel2.setVisible(true);
+
+      panel2.setBounds(0,0,800,600);
+      panel1.setBounds(0,0,800,100);
+
+      panel1.setVisible(true);
+     
    }
 }
