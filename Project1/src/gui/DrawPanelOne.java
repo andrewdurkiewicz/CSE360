5dae116fd21741c7e0629eb1484f9d9f97e9e985
Cody <codyaroberson@outlook.com>
2018-05-25 10:43:11 -0700

    Redesigned icon structure (inprogress)

--- Project1/src/gui/DrawPanelOne.java
+++ Project1/src/gui/DrawPanelOne.java
@@ -6,29 +6,9 @@ import java.awt.Dimension;
 
 public class DrawPanelOne extends JPanel{
 
-private int height, width;
-public OvalLabel oval;
-public TriLabel triangle;
-public RectLabel rectangle;
-public StarLabel star;
-public DiamondLabel diamond;
-
    public DrawPanelOne(){
-      super();
-      super.setVisible(true);
-      super.setBackground(Color.WHITE);
-   
-      oval = new OvalLabel();
-      triangle = new TriLabel();
-      rectangle = new RectLabel();
-      star = new StarLabel();
-      diamond = new DiamondLabel();
-      super.add(oval);
-      super.add(triangle);
-      super.add(rectangle);
-      super.add(star);
-      super.add(diamond);
-      
+      setVisible(true);
+      setBackground(Color.WHITE);
    }
    
 }
