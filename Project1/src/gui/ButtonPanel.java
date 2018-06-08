5b34a8969d72c453bcb5516363bbc7f686c29e87
Andrew Durkiewicz <andrewdurkiewicz@gmail.com>
2018-06-07 18:11:19 -0700

    Adding a significant amount of my portion. need to implement most of it.

--- Project1/src/gui/ButtonPanel.java
+++ Project1/src/gui/ButtonPanel.java
@@ -2,12 +2,20 @@
 package gui;
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
 
 public class ButtonPanel extends JPanel {
 	public ButtonPanel()
 	{
 		setBackground(Color.blue);
+		this.setLayout(new GridLayout(2, 1));
+		this.add(new createTextPanel(), BorderLayout.SOUTH);
 		this.setBorder(BorderFactory.createLineBorder(Color.black));
 	
 	}
--- Project1/src/gui/createTextPanel.java
+++ Project1/src/gui/createTextPanel.java
@@ -2,23 +2,86 @@ package gui;
 
 import java.awt.Button;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
-public class createTextPanel extends JPanel {
+public class createTextPanel extends JPanel{
 
 	public createTextPanel(){
+		this.setLayout(new GridLayout(3,1));
+		JTextArea textInput = new JTextArea("Type here....");
+
+		MouseListener listener = new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				if(textInput.getText().compareTo("Type here....") == 0) {
+					textInput.setText("");
+
+				}
+				else if ((e.getSource() instanceof JButton)) {
+					textInput.setText("");
+
+				}
+			}
+		};
+		textInput.addMouseListener(listener);
+
 		
 		//first button group for changing text
-		setLayout(new FlowLayout());
-		JButton en_italic = new JButton("Italics");
+		JButton en_italic = new JButton("Italic");
 		JButton en_bold = new JButton("Bold");
 		JButton en_underline = new JButton("Underline");
-		this.add(en_italic);
-		this.add(en_bold);
-		this.add(en_underline);
+
+		
+		
+		JPanel editFont = new JPanel();
+		editFont.setLayout(new FlowLayout());
+		editFont.add(en_italic);
+		editFont.add(en_bold);
+		editFont.add(en_underline);
+		
+		
+		JButton complete = new JButton("Complete");
+		JButton clear = new JButton("Clear Text");
+		clear.addMouseListener(listener);
+		//add to panel
+
+		editFont.add(complete);
+		editFont.add(clear);
+		this.add(textInput);
+		this.add(editFont);
 		
 	}
 
